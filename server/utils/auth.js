cb85e18ce0f7488c417ed812f5074f34d4aca55e
Lance Kelley <lancekkcodes@gmail.com>
2022-01-15 04:09:19 -0600

    update auth.js for gql

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,6 +5,35 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  authMiddleware: function({req}) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, {maxAge: expiration});
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+
+  signToken: function({ username, email, _id }) {
+    const payload = { username, email, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  }
+}
+
+/*
+module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
@@ -36,4 +65,4 @@ module.exports = {
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
-};
+};*/
